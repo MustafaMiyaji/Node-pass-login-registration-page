e7480fda12c52f96957b266f761abe6a47366327
Mustafa Alimiyaji <mustafaalimiyaji@gmail.com>
2024-12-19 17:13:21 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://Mustafa:Mustafa786.@test-cluster1.9wd4h.mongodb.net/?retryWrites=true&w=majority&appName=test-cluster1';
+
+module.exports = {
+    mongoURI: dbPassword
+};
